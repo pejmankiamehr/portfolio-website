391682c1bf812adf21ab67415de1c94b24c9fe35
pejman <pejman.kiamehr@gmail.com>
2024-04-23 15:34:05 +0330

    set btn download for cv

--- src/components/header/CvT.jsx
+++ src/components/header/CvT.jsx
@@ -1,13 +1,15 @@
 import { Nav, NavLink } from "react-bootstrap";
+import kiamerper from '/public/images/Pejman_kiamehr_Front_end_(pe)_.pdf'
+import kiameren from '/public/images/Pejman_kiamehr_Front_end_(en).pdf'
 
 function CvT() {
   return (
     <>
       <Nav className=" mt-4 gap-1 gap-md-3 d-flex justify-content-center">
-        <NavLink target="_blank" href="/images/Pejman_kiamehr_Front_end_(per)_.pdf" className="btn">
+        <NavLink  href={kiamerper} className="btn">
           Cv_per
         </NavLink>
-        <NavLink target="_blank" href="/images/Pejman_kiamehr_Front_end_(en)_.pdf" className="btn">
+        <NavLink  href={kiameren} className="btn">
           Cv_en
         </NavLink>
         <NavLink target="_blank" href="/images/certificate.jpg" className="btn">
